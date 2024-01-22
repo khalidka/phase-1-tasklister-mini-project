ee0089b709b252fd43512a34a04e9a5c13c2d62e
root <root@DESKTOP-3S5SIJL.localdomain>
2024-01-22 18:14:15 +0300

    frat:complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formInput = document.getElementById("new-task-description").value;
+    const newTaskItem = document.createElement("li");
+    newTaskItem.textContent = formInput;
+    taskList.appendChild(newTaskItem);
+    document.getElementById("new-task-description").value = "";
+  });
 });
